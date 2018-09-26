a9d06de86e3864dd632b366658bf110064d2664a
sudo-justinwilson <sudo.justin.wilson@gmail.com>
2018-09-26 14:52:22 +1000

    refactor aws-accounts.sh

--- aws-accounts.sh
+++ aws-accounts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# author: Justin Wilson justin.wilson@melbourneit.com.au
 # description: a helper to loop through the profiles that are available in ~/.aws/config.
 
+#CONFIG_PATH=/home/justinwilson/.aws/config
 CONFIG_PATH="${HOME}/.aws/config"
 
 PROFILES=$(egrep "^\[.*\]$" ${CONFIG_PATH} | egrep "([[:alnum:]]|\-)*" -o)
@@ -12,22 +12,29 @@ ARGS=${*}
 
 usage()
 {
-    echo "This script loops through the available profiles in ~/.aws/config and executes the awscli command for"
-    echo "profile."
-    echo "E.G:  if you wanted to get a list of each load-balancer for every profile you have in ~/.aws/config,
-                you could use the following:"
-    echo "$(basename ${0}) elbv2 describe-load-balancers"
-    echo "  the above command would be executed for each profile using the \"--profile\" flag."
-    echo "$(basename ${0}) usage:"
-    echo "$(basename ${0}) --awscli-subcommand --parameter1 [--parameterN..]"
+    echo "      USAGE: $(basename ${0})"
+    echo 
+    echo "      This script loops through the available profiles in ~/.aws/config and executes the awscli command for"
+    echo "      profile."
+    echo "      E.G:  if you wanted to get a list of each load-balancer for every profile you have in ~/.aws/config,
+                      you could use the following:"
+    echo "      $(basename ${0}) elbv2 describe-load-balancers"
+    echo "        the above command would be executed for each profile using the \"--profile\" flag."
+    echo "      $(basename ${0}) usage:"
+    echo "      $(basename ${0}) --awscli-subcommand --parameter1 [--parameterN..]"
     echo
 }
 
 if [[ ${#} < 1 ]]; then
+    echo "Here are the available profiles:";
+    for i in $PROFILES; do
+        echo $i;
+    done;
+elif [[ ${#} = 1 && ${1} = '--help' ]]; then
     usage;
+else
+    for p in $PROFILES; do
+        echo "Results for profile:  ${p}";
+        /usr/bin/aws ${*} --profile ${p};
+    done;
 fi
-
-# execute command for each AWS profile:
-for p in $PROFILES; do
-    /usr/bin/aws ${*} --profile ${p};
-done
